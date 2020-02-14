8d817468aebb6bd9c8ff75d1102bc23796dd14d9
Taras Halaz <taras.halaz@gmail.com>
2020-02-14 14:15:42 +0200

    menuItem: added support of 'removed' property (instead of removing the document)

--- src/modules/menuItem/models/MenuItem.js
+++ src/modules/menuItem/models/MenuItem.js
@@ -9,6 +9,7 @@ const MenuItemSchema = new mongoose.Schema({
   menuId: Types.ObjectId,
   image: String,
   restaurant: Types.ObjectId,
+  removed: {type: Boolean, default: false}
 });
 
 export default mongoose.model('MenuItem', MenuItemSchema);
