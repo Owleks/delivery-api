28629a74a941623f5711b7af39f53fdf9e32cb4d
OJIEG65 <OJIEG65@gmail.com>
2020-02-18 14:25:50 +0200

    add get/update restaurant api

--- src/modules/restaurant/controller.js
+++ src/modules/restaurant/controller.js
@@ -1,19 +1,49 @@
 import RestaurantModel from './models/Restaurant.js';
 
-export const getRestaurantId = async (req, res) => {
-  const { domainName } = req.query;
+export const getRestaurant = async (req, res) => {
+  const { domainName, restaurantId } = req.query;
+  let filter = {};
 
-  if(!domainName) {
-    return res.status(400).send('domainName is required');
+  if(!domainName && !restaurantId) {
+    return res.status(400).send('!domainName || !restaurantId is required');
+  }
+
+  if(domainName) {
+    filter.domainName = domainName;
+  }
+
+  if(restaurantId) {
+    filter._id = restaurantId;
   }
 
   try {
-    const restaurant = await RestaurantModel.findOne({ domainName });
+    const restaurant = await RestaurantModel.findOne(filter);
     if(restaurant) {
       res.status(200).send(restaurant);
     } else {
       res.status(404).send('Restaurant not found');
     }
+  } catch (e) {
+    res.status(500).send('Something went wrong');
+  }
+};
+
+export const updateRestaurant = async (req, res) => {
+  const { restaurantId } = req.params;
+  const { restaurantName } = req.body;
+
+  if(!restaurantName) {
+   return res.status(400).send('restaurantName is required')
+  }
+
+  try {
+    const updatedRestaurant = await RestaurantModel.findByIdAndUpdate(restaurantId, {
+      displayName: restaurantName,
+    }, { new: true, lean: true});
+    if(!updatedRestaurant) {
+      return res.status(404).send('Restaurant not found');
+    }
+    res.status(200).json(updatedRestaurant);
   } catch (e) {
     res.status(500).send('Something went wrong');
   }
--- src/modules/restaurant/router.js
+++ src/modules/restaurant/router.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getRestaurantId } from './controller.js';
+import { getRestaurant, updateRestaurant } from './controller.js';
 import { runAsyncWrapper } from '../../utils/asyncMiddleware.js';
 
 const restaurantRouter = new express.Router();
 
-restaurantRouter.get('/', runAsyncWrapper(getRestaurantId));
+restaurantRouter.get('/', runAsyncWrapper(getRestaurant));
+restaurantRouter.put('/:restaurantId', runAsyncWrapper(updateRestaurant));
 
 export default restaurantRouter;
